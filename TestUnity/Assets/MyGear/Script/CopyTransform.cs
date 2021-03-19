70ba0badfaaa1bbbdee83f6d960df4617f7c7bc0
TOKIOatoz <tokioatoz1@gmail.com>
2021-03-20 03:17:14 +0900

    add script to cancel HMD's position and rotation.

---
+++ TestUnity/Assets/MyGear/Script/CopyTransform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyTransform : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameObject1 = default;
+    private Vector3 thisPosition = default;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        thisPosition.x = gameObject1.transform.localPosition.x;
+        thisPosition.y = gameObject1.transform.localPosition.y;
+        thisPosition.z = gameObject1.transform.localPosition.z;
+        this.transform.localPosition = thisPosition;
+        this.transform.localRotation = gameObject1.transform.localRotation;
+    }
+}
